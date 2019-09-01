96cf66a2d2b97eea26820772bf518f778ddcdfd4
jez <jeremy+github@jeremystevens.net>
2019-09-01 18:52:35 +0100

    moved main activity into a feature package

--- app/src/main/java/com/ninjarific/whereiroam/features/home/MainActivity.kt
+++ app/src/main/java/com/ninjarific/whereiroam/features/home/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.ninjarific.whereiroam
+package com.ninjarific.whereiroam.features.home
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.ninjarific.whereiroam.R
 
 import kotlinx.android.synthetic.main.activity_main.*
 
