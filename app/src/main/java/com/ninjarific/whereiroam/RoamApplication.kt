1177ce7db2b1bd34734e34c4f58513010e876e10
jez <jeremy+github@jeremystevens.net>
2019-09-01 19:13:20 +0100

    added Timber for logging

--- app/src/main/java/com/ninjarific/whereiroam/RoamApplication.kt
+++ app/src/main/java/com/ninjarific/whereiroam/RoamApplication.kt
@@ -3,13 +3,18 @@ package com.ninjarific.whereiroam
 import android.app.Application
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
+import timber.log.Timber
 
 class RoamApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        Timber.plant(Timber.DebugTree())
+
         startKoin {
             androidContext(this@RoamApplication)
             modules(roamModule)
         }
+
+        Timber.i("app created")
     }
 }
