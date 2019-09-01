e71daace4c6a832b60960d92b13f8cbf752e33f0
jez <jeremy+github@jeremystevens.net>
2019-09-01 18:51:57 +0100

    expanded database implementation

--- app/src/androidTest/java/com/ninjarific/whereiroam/database/TripDatabaseTest.kt
+++ app/src/androidTest/java/com/ninjarific/whereiroam/database/TripDatabaseTest.kt
@@ -17,12 +17,16 @@ class DatabaseTest {
 
     private lateinit var db: TripDatabase
     private lateinit var tripDao: TripDao
+    private lateinit var visitDao: VisitDetailsDao
+    private lateinit var tripDetailsDao: TripDetailsDao
 
     @Before
     fun setUp() {
         val context = ApplicationProvider.getApplicationContext<Context>()
         db = Room.inMemoryDatabaseBuilder(context, TripDatabase::class.java).build()
         tripDao = db.tripDao()
+        visitDao = db.visitDetailsDao()
+        tripDetailsDao = db.tripDetailsDao()
     }
 
     @After
@@ -31,24 +35,92 @@ class DatabaseTest {
     }
 
     @Test
-    fun insertReadDeleteTrip() = runBlocking {
+    fun insertReadUpdateDeleteTrip() = runBlocking {
         val trip = Trip(
-            null,
+            0,
             "title",
             "countryCode",
-            "start",
-            "end"
+            0,
+            10
         )
-        tripDao.insertTrip(trip)
-
-        val trips = tripDao.getAllTrips()
+        val tripId = tripDao.insert(trip)
+        val trips = tripDao.getAll()
         assertEquals(1, trips.size)
+        assertEquals(trip.copy(uid = tripId), trips[0])
 
-        assertEquals(trip.copy(uid = 1), trips[0])
+        val updatedTrip = trips[0].copy(title="updatedTitle")
+        tripDao.update(updatedTrip)
+        val updatedTrips = tripDao.getAll()
+        assertEquals(1, updatedTrips.size)
+        assertEquals(updatedTrip, updatedTrips[0])
 
-        tripDao.deleteTrip(trips[0])
-
-        val tripsAfterDelete = tripDao.getAllTrips()
+        tripDao.delete(updatedTrip)
+        val tripsAfterDelete = tripDao.getAll()
         assertEquals(0, tripsAfterDelete.size)
+    }
+
+    @Test
+    fun insertReadUpdateDeleteVisitDetails() = runBlocking {
+        val trip = Trip(
+            0,
+            "title",
+            "countryCode",
+            0,
+            10
+        )
+        val tripId = tripDao.insert(trip)
+
+        val visit = VisitDetails(
+            0,
+            tripId,
+            "visitCountry",
+            1,
+            2
+        )
+        val visitId = visitDao.insert(visit)
+
+        val visits = visitDao.getVisits(tripId)
+        assertEquals(1, visits.size)
+        assertEquals(visit.copy(uid = visitId), visits[0])
+
+        val updatedVisit = visits[0].copy(countryCode="updatedCountryCode")
+        visitDao.update(updatedVisit)
+        val updatedVisits = visitDao.getVisits(tripId)
+        assertEquals(1, updatedVisits.size)
+        assertEquals(updatedVisit, updatedVisits[0])
+
+        visitDao.delete(updatedVisit)
+        val visitsAfterDelete = visitDao.getVisits(tripId)
+        assertEquals(0, visitsAfterDelete.size)
+
+        // verify trip unaffected
+        val trips = tripDao.getAll()
+        assertEquals(1, trips.size)
+        assertEquals(trip.copy(uid = tripId), trips[0])
+    }
+
+    @Test
+    fun tripDetails() = runBlocking {
+        val tripId = 1L
+        val trip = Trip(
+            tripId,
+            "title",
+            "countryCode",
+            0,
+            10
+        )
+        tripDao.insert(trip)
+        val visit = VisitDetails(
+            1,
+            tripId,
+            "visitCountry",
+            1,
+            2
+        )
+        visitDao.insert(visit)
+
+        val tripDetails = tripDetailsDao.get(tripId)
+        assertEquals(trip.copy(uid=tripId), tripDetails.trip)
+        assertEquals(visit.copy(uid=1), tripDetails.visitDetails[0])
     }
 }
--- app/src/main/java/com/ninjarific/whereiroam/database/TripDatabase.kt
+++ app/src/main/java/com/ninjarific/whereiroam/database/TripDatabase.kt
@@ -1,40 +1,82 @@
 package com.ninjarific.whereiroam.database
 
 import androidx.room.*
+import androidx.room.ForeignKey.CASCADE
 
 @Database(entities = [Trip::class, VisitDetails::class], version = 1)
 abstract class TripDatabase : RoomDatabase() {
     abstract fun tripDao(): TripDao
+    abstract fun visitDetailsDao(): VisitDetailsDao
+    abstract fun tripDetailsDao(): TripDetailsDao
+}
+
+@Dao
+interface TripDao {
+    @Query("SELECT * FROM trip order by trip.trip_start")
+    suspend fun getAll(): List<Trip>
+
+    @Insert
+    suspend fun insert(trip: Trip) : Long
+
+    @Update
+    suspend fun update(trip: Trip)
+
+    @Delete
+    suspend fun delete(trip: Trip)
 }
 
 @Entity
 data class Trip(
-    @PrimaryKey(autoGenerate = true) val uid: Int?,
+    @PrimaryKey(autoGenerate = true) val uid: Long = 0,
     @ColumnInfo(name="title") val title: String,
     @ColumnInfo(name="first_country_code") val firstCountryCode: String,
-    @ColumnInfo(name="trip_start") val tripStart: String,
-    @ColumnInfo(name="trip_end") val tripEnd: String?
+    @ColumnInfo(name="trip_start") val tripStart: Long,
+    @ColumnInfo(name="trip_end") val tripEnd: Long?
 )
 
 @Dao
-interface TripDao {
-    @Query("SELECT * FROM trip")
-    suspend fun getAllTrips(): List<Trip>
+interface VisitDetailsDao {
+    @Query("SELECT * FROM visitDetails WHERE trip_id=:tripId")
+    suspend fun getVisits(tripId: Long): List<VisitDetails>
 
     @Insert
-    suspend fun insertTrip(trip: Trip)
+    suspend fun insert(visit: VisitDetails) : Long
 
     @Update
-    suspend fun updateTrip(trip: Trip)
+    suspend fun update(visit: VisitDetails)
 
     @Delete
-    suspend fun deleteTrip(trip: Trip)
+    suspend fun delete(visit: VisitDetails)
 }
 
-@Entity
+@Entity(foreignKeys = [
+    ForeignKey(
+        entity = Trip::class,
+        parentColumns = ["uid"],
+        childColumns = ["trip_id"],
+        onDelete = CASCADE,
+        onUpdate = CASCADE
+        )
+])
 data class VisitDetails(
-    @PrimaryKey val uid: Int,
+    @PrimaryKey(autoGenerate = true) val uid: Long = 0,
+    @ColumnInfo(name="trip_id", index=true) val trip: Long,
     @ColumnInfo(name="country_code") val countryCode: String,
-    @ColumnInfo(name="start") val start: String,
-    @ColumnInfo(name="end") val end: String
+    @ColumnInfo(name="start") val start: Long,
+    @ColumnInfo(name="end") val end: Long
 )
+
+@Dao
+interface TripDetailsDao {
+    @Transaction
+    @Query("SELECT * FROM trip WHERE uid = :tripId")
+    suspend fun get(tripId: Long): TripDetails
+}
+
+class TripDetails {
+    @Embedded
+    var trip: Trip? = null
+
+    @Relation(parentColumn = "uid", entityColumn = "trip_id")
+    var visitDetails: List<VisitDetails> = emptyList()
+}
