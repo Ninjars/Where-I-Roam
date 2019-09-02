5e13416723d622b2e193d62bc0d23cdb25e96d59
jez <jeremy+github@jeremystevens.net>
2019-09-02 22:54:08 +0100

    jake warton's port of java 8 time lib

--- app/src/main/java/com/ninjarific/whereiroam/RoamApplication.kt
+++ app/src/main/java/com/ninjarific/whereiroam/RoamApplication.kt
@@ -1,6 +1,7 @@
 package com.ninjarific.whereiroam
 
 import android.app.Application
+import com.jakewharton.threetenabp.AndroidThreeTen
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 import timber.log.Timber
@@ -9,6 +10,8 @@ class RoamApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
+
+        AndroidThreeTen.init(this)
 
         startKoin {
             androidContext(this@RoamApplication)
